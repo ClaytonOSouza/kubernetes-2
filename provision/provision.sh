5f4892cd34b095eb23f8b16e419cca3248ce50ac
Hector Vido <39673799+hector-vido@users.noreply.github.com>
2019-10-09 08:07:29 -0300

    Adicionado comandos para modificar o iptables

--- provision/provision.sh
+++ provision/provision.sh
@@ -39,3 +39,8 @@ systemctl restart docker
 sed -Ei 's/(.*swap.*)/#\1/g' /etc/fstab
 swapoff -a
 usermod -G docker -a vagrant
+
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+update-alternatives --set arptables /usr/sbin/arptables-legacy
+update-alternatives --set ebtables /usr/sbin/ebtables-legacy
